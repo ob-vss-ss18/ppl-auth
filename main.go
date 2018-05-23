3e05ecb89d6a4ff4cfe532644002eeaf1b089866
Marita Segl <maritasegl@gmx.de>
2018-05-23 13:25:55 +0200

    Add postgres and migration framework

--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
 	"os"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/postgres"
+	_ "github.com/golang-migrate/migrate/source/file"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	db, err := sql.Open("postgres", "postgres://postgres:test123@localhost:5432/ppl-auth?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations/",
+		"postgres", driver)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m.Up()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	http.HandleFunc("/", hello)
-	err := http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
